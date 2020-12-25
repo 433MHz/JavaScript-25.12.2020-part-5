4f14019c51ff2f7358e0e50498cf5a0cb29be2bc
Krystian <user@DESKTOP-HNKK2FJ>
2020-12-25 23:57:43 +0100

    added javascript function to make divs with alphabet letters as buttons on page automaticly

--- script.js
+++ script.js
@@ -12,6 +12,33 @@ function insertText(){
     }
 
     document.getElementById("logo").innerHTML = haslo1;
+    insertDivs();
 }
 
 window.onload = insertText;
+
+
+function insertDivs() {
+    var div_value = "A Ą B C Ć D E Ę F G H I J K L Ł M N Ń O Ó P Q R S Ś T U V W X Y Z Ź Ż";
+    var div_html = "";
+    var div_clear = "<div style=\"clear: both;\"></div>"
+    var counter = 0;
+    var y = 7;
+
+    
+        for(x = 1; x <=div_value.length; x++){
+            if(div_value.charAt(x)==" ");
+
+            else{
+                var temporaryDivHtml = "<div class=\"button\">" + div_value.charAt(counter) + "</div>";
+                div_html = div_html + temporaryDivHtml;
+                counter = counter + 2;
+            } 
+            if((counter/2) == y){
+
+                y = y +7
+                div_html = div_html + div_clear;
+            }
+        }
+    document.getElementById("right").innerHTML = div_html;
+}
